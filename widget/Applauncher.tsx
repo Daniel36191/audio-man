2c09081b5eaef5c0e4a13b95dbd04561cdbc2eb9
Daniel36191 <dmoeller3998@gmail.com>
2026-03-02 20:25:33 -0500

    Volume Control and theme added

--- widget/Applauncher.tsx
+++ widget/Applauncher.tsx
@@ -1,87 +1,43 @@
-import { For, createState } from "ags";
 import { Astal, Gtk, Gdk } from "ags/gtk4";
-import AstalApps from "gi://AstalApps";
-import { CellLayoutNamespace } from '../@girs/gtk-3.0';
-import { exit } from "system";
 import VolumeSlider from './modules/slider';
 
-const { TOP, BOTTOM, LEFT, RIGHT } = Astal.WindowAnchor;
+const { TOP, BOTTOM, LEFT, RIGHT } = Astal.WindowAnchor
 
-// Main func
 export default function Applauncher() {
-	let win: Astal.Window;
-	let contentbox: Gtk.Box
+  let win: Astal.Window
 
-  function onKey(
-    _e: Gtk.EventControllerKey,
-    keyval: number,
-    _: number,
-    __: number,
-  ) {
+  function onKey(_e: Gtk.EventControllerKey, keyval: number) {
     if (keyval === Gdk.KEY_Escape) {
       win.visible = false
       return true
     }
-		return false
+    return false
   }
 
-	function onSliderChanged(
-		id: string,
-		value: number
-	) {
-		print(value + id)
-	}
-
-
-	return (
-		<window
-			$={(ref) => {win = ref}}
-			name="launcher"
-			anchor={TOP | BOTTOM | LEFT | RIGHT}
-			exclusivity={Astal.Exclusivity.IGNORE}
-			keymode={Astal.Keymode.EXCLUSIVE}
-			canFocus={true}
-		>
-			<Gtk.EventControllerKey onKeyPressed={onKey} />
-			<box
-				$={(ref) => {contentbox = ref}}
-				name="launcher-content"
-				valign={Gtk.Align.CENTER}
-				halign={Gtk.Align.CENTER}
-				orientation={Gtk.Orientation.HORIZONTAL}
-				spacing={20}
-				widthRequest={400}
-				heightRequest={300}
-				canFocus={true}
-			>
-				<VolumeSlider
-					id = {12}
-					label = {"Spotify"}
-					onChange = {(value) => onSliderChanged("Spotify-Volume", value)}
-					initialValue = {25}
-					/>
-
-				<VolumeSlider
-					id = {12}
-					label = {"Comms"}
-					onChange = {(value) => onSliderChanged("Comms-Volume", value)}
-					initialValue = {25}
-					/>
-
-				<VolumeSlider
-					id = {12}
-					label = {"Mic"}
-					onChange = {(value) => onSliderChanged("Mic-Output", value)}
-					initialValue = {25}
-					/>
-
-				<VolumeSlider
-					id = {12}
-					label = {"General"}
-					onChange = {(value) => onSliderChanged("General-Volume", value)}
-					initialValue = {25}
-					/>
-			</box>
-		</window>
-	)
+  return (
+    <window
+      $={(ref) => { win = ref }}
+      name="launcher"
+      anchor={TOP | BOTTOM | LEFT | RIGHT}
+      exclusivity={Astal.Exclusivity.IGNORE}
+      keymode={Astal.Keymode.EXCLUSIVE}
+      canFocus={true}
+    >
+      <Gtk.EventControllerKey onKeyPressed={onKey} />
+      <box
+        name="launcher-content"
+        valign={Gtk.Align.CENTER}
+        halign={Gtk.Align.CENTER}
+        orientation={Gtk.Orientation.HORIZONTAL}
+        spacing={20}
+        widthRequest={400}
+        heightRequest={300}
+      >
+        <VolumeSlider wpctlId={37} label="Spotify" />
+        <VolumeSlider wpctlId={39} label="Comms" />
+        <VolumeSlider wpctlId={35} label="Mic" />
+        <VolumeSlider wpctlId={34} label="General" />
+      </box>
+    </window>
+  )
 }
--- widget/modules/slider.tsx
+++ widget/modules/slider.tsx
@@ -1,58 +1,54 @@
-import { Astal, Gtk, Gdk } from "ags/gtk4";
-import { createState } from "ags";
+import { Gtk } from "ags/gtk4";
+import GLib from "gi://GLib";
 
 interface VolumeSliderProps {
-  id?: number
-  initialValue?: number
+  wpctlId: number
   onChange?: (value: number) => void
   label?: string
 }
 
-export default function VolumeSlider(props: ReadOnly<VolumeSliderProps>) {
-  const [volume, setVolume] = createState<number>(props.initialValue ?? 0)
+function getCurrentVolume(wpctlId: number): number {
+  try {
+    const [ok, stdout] = GLib.spawn_command_line_sync(`wpctl get-volume ${wpctlId}`)
+    if (!ok || !stdout) return 50
+    const match = new TextDecoder().decode(stdout).match(/Volume:\s*([\d.]+)/)
+    return match ? Math.round(parseFloat(match[1]) * 100) : 50
+  } catch {
+    return 50
+  }
+}
 
-	let valueLabel: Gtk.Label
+export default function VolumeSlider({ wpctlId, label, onChange }: ReadOnly<VolumeSliderProps>) {
+  const initialVol = getCurrentVolume(wpctlId)
 
   function onSliderChanged(slider: Gtk.Scale) {
-		const newValue = Math.round(slider.get_value())
-    setVolume(newValue)
-
-		if (props.onChange) {
-			props.onChange(newValue)
-		}
-
-		if (valueLabel) {
-			valueLabel.label = `${props.label}`
-		}
+    const value = Math.round(slider.get_value())
+    GLib.spawn_command_line_async(`wpctl set-volume ${wpctlId} ${(value / 100).toFixed(2)}`)
+    onChange?.(value)
   }
 
-return (
-		<box
-			orientation={Gtk.Orientation.VERTICAL}
-			spacing={10}
-			halign={Gtk.Align.CENTER}
-			heightRequest={250}
-			widthRequest={120}
-			>
-			<label
-				$={(ref) => {valueLabel = ref}}
-				label={`${props.label}`}
-				halign={Gtk.Align.CENTER}
-				widthRequest={100}
-			/>
-			<Gtk.Scale
-				$={(ref) => {
-					ref.set_range(0, 100);
-					ref.inverted = true;
-					ref.set_value(volume)
-				}}
-				orientation={Gtk.Orientation.VERTICAL}
-				halign={Gtk.Align.CENTER}
-				drawValue={false}
-				onChangeValue={onSliderChanged}
-				heightRequest={200}
-				widthRequest={100}
-			/>
-		</box>
-	);
+  return (
+    <box
+      orientation={Gtk.Orientation.VERTICAL}
+      spacing={10}
+      halign={Gtk.Align.CENTER}
+      heightRequest={250}
+      widthRequest={120}
+    >
+      <label label={label} halign={Gtk.Align.CENTER} widthRequest={100} />
+      <Gtk.Scale
+        $={(ref) => {
+          ref.set_range(0, 100)
+          ref.inverted = true
+          ref.set_value(initialVol)
+        }}
+        orientation={Gtk.Orientation.VERTICAL}
+        halign={Gtk.Align.CENTER}
+        drawValue={false}
+        onChangeValue={onSliderChanged}
+        heightRequest={200}
+        widthRequest={100}
+      />
+    </box>
+  )
 }
