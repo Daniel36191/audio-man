e41730ecf1480500517f3c3a657c6348c610594c
Daniel36191 <dmoeller3998@gmail.com>
2026-03-02 20:52:36 -0500

    Fully working poc

--- widget/Applauncher.tsx
+++ widget/Applauncher.tsx
@@ -1,3 +1,4 @@
+import app from "ags/gtk4/app";
 import { Astal, Gtk, Gdk } from "ags/gtk4";
 import VolumeSlider from './modules/slider';
 
@@ -6,9 +7,13 @@ const { TOP, BOTTOM, LEFT, RIGHT } = Astal.WindowAnchor
 export default function Applauncher() {
   let win: Astal.Window
 
+  function close() {
+    app.quit()
+  }
+
   function onKey(_e: Gtk.EventControllerKey, keyval: number) {
     if (keyval === Gdk.KEY_Escape) {
-      win.visible = false
+      close()
       return true
     }
     return false
@@ -24,20 +29,37 @@ export default function Applauncher() {
       canFocus={true}
     >
       <Gtk.EventControllerKey onKeyPressed={onKey} />
-      <box
-        name="launcher-content"
-        valign={Gtk.Align.CENTER}
-        halign={Gtk.Align.CENTER}
-        orientation={Gtk.Orientation.HORIZONTAL}
-        spacing={20}
-        widthRequest={400}
-        heightRequest={300}
-      >
-        <VolumeSlider wpctlId={37} label="Spotify" />
-        <VolumeSlider wpctlId={39} label="Comms" />
-        <VolumeSlider wpctlId={35} label="Mic" />
-        <VolumeSlider wpctlId={34} label="General" />
-      </box>
+      <overlay>
+        {/* Transparent click-off layer */}
+        <button
+          name="launcher-dismiss"
+          vexpand={true}
+          hexpand={true}
+          onClicked={close}
+        />
+        {/* Actual content */}
+				<box
+					name="launcher-content"
+					valign={Gtk.Align.CENTER}
+					halign={Gtk.Align.CENTER}
+					orientation={Gtk.Orientation.VERTICAL}
+					spacing={8}
+				>
+					<button name="launcher-close" halign={Gtk.Align.END} onClicked={close}>
+						<label label="" />
+					</button>
+					<box
+						orientation={Gtk.Orientation.HORIZONTAL}
+						spacing={8}
+						heightRequest={320}
+					>
+						<VolumeSlider wpctlId={37} label="Spotify" icon="󰓇" />
+						<VolumeSlider wpctlId={39} label="Comms"   icon="" />
+						<VolumeSlider wpctlId={35} label="Mic"     icon="" />
+						<VolumeSlider wpctlId={34} label="General" icon="󰕾" />
+					</box>
+				</box>
+      </overlay>
     </window>
   )
 }
--- widget/modules/slider.tsx
+++ widget/modules/slider.tsx
@@ -1,10 +1,13 @@
+// slider.tsx
 import { Gtk } from "ags/gtk4";
+import { createState } from "ags";
 import GLib from "gi://GLib";
 
 interface VolumeSliderProps {
   wpctlId: number
   onChange?: (value: number) => void
   label?: string
+  icon?: string
 }
 
 function getCurrentVolume(wpctlId: number): number {
@@ -17,25 +20,31 @@ function getCurrentVolume(wpctlId: number): number {
     return 50
   }
 }
-
-export default function VolumeSlider({ wpctlId, label, onChange }: ReadOnly<VolumeSliderProps>) {
+	
+export default function VolumeSlider({ wpctlId, label, icon, onChange }: ReadOnly<VolumeSliderProps>) {
   const initialVol = getCurrentVolume(wpctlId)
+  const [volume, setVolume] = createState<number>(initialVol)
 
   function onSliderChanged(slider: Gtk.Scale) {
     const value = Math.round(slider.get_value())
+    setVolume(value)
     GLib.spawn_command_line_async(`wpctl set-volume ${wpctlId} ${(value / 100).toFixed(2)}`)
     onChange?.(value)
   }
 
   return (
     <box
+      name="slider-card"
       orientation={Gtk.Orientation.VERTICAL}
-      spacing={10}
+      spacing={8}
       halign={Gtk.Align.CENTER}
-      heightRequest={250}
-      widthRequest={120}
+      valign={Gtk.Align.FILL}
+      widthRequest={90}
     >
-      <label label={label} halign={Gtk.Align.CENTER} widthRequest={100} />
+      <box orientation={Gtk.Orientation.HORIZONTAL} spacing={6} halign={Gtk.Align.CENTER}>
+        <label name="slider-icon" label={icon ?? ""} />
+        <label name="slider-label" label={label ?? ""} />
+      </box>
       <Gtk.Scale
         $={(ref) => {
           ref.set_range(0, 100)
@@ -44,10 +53,15 @@ export default function VolumeSlider({ wpctlId, label, onChange }: ReadOnly<Volu
         }}
         orientation={Gtk.Orientation.VERTICAL}
         halign={Gtk.Align.CENTER}
+        vexpand={true}
         drawValue={false}
         onChangeValue={onSliderChanged}
-        heightRequest={200}
-        widthRequest={100}
+        widthRequest={40}
+      />
+      <label
+        name="slider-percent"
+        label={volume.as(v => `${v}%`)}
+        halign={Gtk.Align.CENTER}
       />
     </box>
   )
